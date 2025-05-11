d81a19317090e8986ec84ace30041ad0f117a3c1
Marcello Rossi <marcellorossi98@gmail.com>
2025-05-11 07:37:17 -0400

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,6 +1,7 @@
 import os
 from redis import Redis
-from rq import Worker, Queue, Connection
+from rq import Worker, Queue
+from rq.connections import Connection
 
 listen = ['default']
 redis_url = (
